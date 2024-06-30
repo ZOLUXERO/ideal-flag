7d92dcb6fdddcdc394e3f9b401e754c1a1be5221
Aureliano Buendia <zoluxero@gmail.com>
2024-06-30 10:07:45 -0500

    feat(ideal-flag): cross origin allow

--- src/main/java/com/featureflagmanagment/featureflagmanagment/controller/ProjectsController.java
+++ src/main/java/com/featureflagmanagment/featureflagmanagment/controller/ProjectsController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Collections;
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequiredArgsConstructor
 @Slf4j
